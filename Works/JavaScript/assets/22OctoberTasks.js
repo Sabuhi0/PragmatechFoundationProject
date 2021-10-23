c4b1089177e2d55a90dd5d9b093b2e615b80969a
Sabuhi0 <sabuhiq0@gmail.com>
2021-10-24 00:58:48 +0400

    Task 1 completed

---
+++ Works/JavaScript/assets/22OctoberTasks.js
@@ -0,0 +1,10 @@
+// 22 October tasks;
+
+// 1) 2 ededi tipində məlumatı parametr olaraq qəbul edib ededlərin hasilini return edən funksiya yazın
+let test = hasil(3, 5);
+
+function hasil(a, b) {
+    return a * b
+}
+
+console.log(test);
