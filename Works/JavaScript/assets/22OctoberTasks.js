cb1b66fbc5d4b42a0823bff7e50e21fc47456507
Sabuhi0 <sabuhiq0@gmail.com>
2021-10-24 23:45:07 +0400

    Task 3 completed

--- Works/JavaScript/assets/22OctoberTasks.js
+++ Works/JavaScript/assets/22OctoberTasks.js
@@ -1,8 +1,8 @@
 // 22 October tasks;
 
-// Task 1
+// Task 1;
 
-// 1) 2 ededi tipində məlumatı parametr olaraq qəbul edib ededlərin hasilini return edən funksiya yazın
+// 1) 2 ededi tipində məlumatı parametr olaraq qəbul edib ededlərin hasilini return edən funksiya yazın;
 /*let test = hasil(3, 5);
 
 function hasil(a, b) {
@@ -11,14 +11,26 @@ function hasil(a, b) {
 
 console.log(test);*/
 
-// Task 2
+// Task 2;
 
-// 2) 1 eded parametr qəbul edib daxil edilən dəyərin 1 artığını return elətdirən funksiya yazın
+// 2) 1 eded parametr qəbul edib daxil edilən dəyərin 1 artığını return elətdirən funksiya yazın;
 
-let num = parm(5);
+/*let num = parm(5);
 
 function parm(c) {
     return ++c
 }
 
-console.log(num)
+console.log(num);*/
+
+// Task 3;
+
+// 3) Daxil edilən 5 parametri array formasında return edən funksiya yazın;
+
+let array = param(["Salam", "Bb", "JS", "C#", "PHP"]);
+
+function param(param) {
+    return param
+}
+
+console.log(array);
