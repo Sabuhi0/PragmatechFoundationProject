62fce54833b45f1d4385921a465c0b6617971540
Sabuhi0 <sabuhiq0@gmail.com>
2021-10-26 16:59:39 +0400

    Slider with html dom

--- Works/JavaScript/October26Tasks/assets/js/tasks.js
+++ Works/JavaScript/October26Tasks/assets/js/tasks.js
@@ -1,1 +1,18 @@
-/* Initial commit */
+let x = 0;
+let containerWidth = document.querySelector('.slider-container').clientWidth;
+let slides = document.querySelectorAll('.slide-item');
+for (let i = 0; i < slides.length; i++) {
+    slides[i].style.width = `${containerWidth/2}px`
+}
+document.querySelector('.slide-item').style.width = `${containerWidth/2}px`
+let box = document.querySelector('.slide-items');
+
+function left() {
+    x = x - (containerWidth / 2 + 20);
+    box.style.left = `${x}px`;
+}
+
+function right() {
+    x = x + (containerWidth / 2 + 20);
+    box.style.left = `${x}px`;
+}
