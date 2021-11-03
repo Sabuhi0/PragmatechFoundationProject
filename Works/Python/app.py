54dca9c9ea789471806592655a7559d20fa1dfaa
Sabuhi0 <sabuhiq0@gmail.com>
2021-11-03 23:56:23 +0400

    Pythondan istifadə edərək istifadəçinin yaşını öyrəndim

--- Works/Python/app.py
+++ Works/Python/app.py
@@ -1,4 +1,16 @@
 # Inital commit
 
-a = input("Adiniz:")
-print(a + " necesen")
+# a = input("Adiniz:")
+# print(a + " necesen")
+
+"""
+Dictionaries istifadə edərək istifadəçinin yaşını öyrəndim
+"""
+user = {
+    "name":"Ali",
+    "surname":"Aliyev",
+    "age":21,
+}
+
+print(user['age'])
+
