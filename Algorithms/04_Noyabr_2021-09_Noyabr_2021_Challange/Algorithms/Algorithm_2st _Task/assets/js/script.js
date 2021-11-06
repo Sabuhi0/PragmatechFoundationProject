57a495e145efb2cc6004b5edda8e494f89e0cdcf
Sabuhi0 <sabuhiq0@gmail.com>
2021-11-06 22:22:50 +0400

    Algorithm 2st task completed

--- Algorithms/04_Noyabr_2021-09_Noyabr_2021_Challange/Algorithms/Algorithm_2st _Task/assets/js/script.js
+++ Algorithms/04_Noyabr_2021-09_Noyabr_2021_Challange/Algorithms/Algorithm_2st _Task/assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+// console.log("Hello")
+
+let num = prompt("eded daxil edin")
+Number("x")
+let full = parseInt(num)
+let a = num - full
+let b = a.toPrecision(2)
+
+let result = (100 * b) + full
+document.write(result)
