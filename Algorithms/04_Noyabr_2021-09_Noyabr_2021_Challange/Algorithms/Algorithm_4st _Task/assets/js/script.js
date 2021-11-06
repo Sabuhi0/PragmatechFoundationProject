fbfb7aeb6d6b591c990948872d082f5ef359a475
Sabuhi0 <sabuhiq0@gmail.com>
2021-11-07 01:14:52 +0400

    Algorithm 4st task completed

---
+++ Algorithms/04_Noyabr_2021-09_Noyabr_2021_Challange/Algorithms/Algorithm_4st _Task/assets/js/script.js
@@ -0,0 +1,7 @@
+// Verilmiş arreyin uzunluğunu elementlərini çıxarmaq. numbers = [45, 4, 9, 16, 25];
+
+let numbers = [45, 4, 9, 16, 25];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(`${i}` + "." + numbers[i])
+}
