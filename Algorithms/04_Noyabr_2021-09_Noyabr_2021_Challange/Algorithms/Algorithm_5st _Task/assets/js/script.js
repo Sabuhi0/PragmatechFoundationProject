bf66f91825563b65833a1338380b0e334086af0e
Sabuhi0 <sabuhiq0@gmail.com>
2021-11-07 01:36:13 +0400

    Algorithm 5st task completed

---
+++ Algorithms/04_Noyabr_2021-09_Noyabr_2021_Challange/Algorithms/Algorithm_5st _Task/assets/js/script.js
@@ -0,0 +1,7 @@
+// 2 ayrı arreyi birləşdirmək.
+
+const foundation012 = ["Rahman", "Elmar", "Ravi", "Fezail", "Vesile"];
+const newFoundation012 = ["Aysel", "Murad"];
+const children = foundation012.concat(newFoundation012);
+
+console.log(`List = ` + children);
