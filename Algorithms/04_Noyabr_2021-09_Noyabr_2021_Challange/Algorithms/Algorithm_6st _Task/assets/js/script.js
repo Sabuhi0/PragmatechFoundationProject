fe7251cc31a4a03aeb6d99f27850e8fb8f5a316e
Sabuhi0 <sabuhiq0@gmail.com>
2021-11-07 14:53:57 +0400

    Algorithm 6st task completed

---
+++ Algorithms/04_Noyabr_2021-09_Noyabr_2021_Challange/Algorithms/Algorithm_6st _Task/assets/js/script.js
@@ -0,0 +1,10 @@
+// Rənglərə aid arrey yaradib arrey-ə kənardan əlavə söz əlavə etmək.
+
+let colors = ["Red", "Blue", "Black", "Yellow"]
+colors.push("Kenardan soz elave etdim)")
+
+console.log(colors)
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i])
+}
