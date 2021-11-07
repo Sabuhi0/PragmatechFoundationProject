bf3568e03344a383b6de30b01dabfb86135e5c1f
Sabuhi0 <sabuhiq0@gmail.com>
2021-11-07 14:55:36 +0400

    Algorithm 7st task completed

---
+++ Algorithms/04_Noyabr_2021-09_Noyabr_2021_Challange/Algorithms/Algorithm_7st _Task/assets/js/script.js
@@ -0,0 +1,9 @@
+// Yeni bir obyekt yaradib onun elementlərini console log da çıxarmaq. (Məsələn, Taski şəxsin adı Aydan, Yaşı qadının yaşı soruşulmaz, vəzifəsi Tələbə koordinatorudur)
+
+let user = {
+    Ad: "Aydan",
+    Yas: "Qadının yaşı soruşulmaz",
+    Vəzifəsi: "Tələbə koordinatorudur)"
+}
+
+console.log(user)
