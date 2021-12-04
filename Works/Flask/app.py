2d838010f653506c9be0cf9b4520085349880090
sabuhiq0@gmail.com <sabuhiq0@gmail.com>
2021-12-05 01:07:28 +0400

    Add flask file

---
+++ Works/Flask/app.py
@@ -0,0 +1,33 @@
+import re
+from flask import Flask,redirect,url_for,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(100))
+    content=db.Column(db.Text)
+    complete=db.Column(db.Boolean)
+
+@app.route("/")
+def index():
+    todos = Todo.query.all()
+    return render_template("index.html", todos=todos)
+
+@app.route("/add",methods=["GET","POST"])   
+def add():
+    title=request.form.get("title")
+    content=request.form.get("content")
+    newTodo=Todo(title=title,content=content,complete = False)
+    db.session.add(newTodo)
+    db.session.commit()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
